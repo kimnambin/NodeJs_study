3b8d0768d55e8b0a595ccebb2f154772c5abc47c
kimnambin <mkkim044@naver.com>
2024-02-01 13:49:36 +0900

    02월01일

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,65 @@
-//io는 자동적으로 소켓 io 벡엔드와 연결해주는 함수임
-const socket = io();
 
-const welcome = document.getElementById("welcome");
+const socket = io();//io는 자동적으로 소켓 io 벡엔드와 연결해주는 함수임
+const welcome = document.getElementById("welcome");//처음 보여지는 부분
 const form = welcome.querySelector("form");
+const room = document.getElementById("room"); //방에 입장하고 나서 보여지는 부분
 
-function handleRoomSubmit(event){
+room.hidden = true; //방 숨기기
+
+let roomName; //방에 입장한 유저들 닉네임
+
+//메시지를 추가해주는 함수
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+//메시지 전송 (채팅)함수
+function handleMessageSubmit(event){
     event.preventDefault();
-    const input = form.querySelector("input");
-    //룸은 보내는 것이 아닌 emit 해주면 된다함
-    socket.emit("enter_room" , { payload: input.value },
-    ()=> {
-        console.log("server is done 준비됐구~");
+    const input = room.querySelector("#msg input");//메시지 내용 보여주기
+    const value = input.value;
+    socket.emit("new_message" , input.value , roomNmae, () =>{
+        addMessage(`유:${value}`);
     });
     input.value = "";
 }
 
+//닉네임 설정부분
+
+//방에서 보여지는 부분
+function showRoom(){ 
+    welcome.hidden = true;
+    room.hidden = false;
+    const h3 = room.querySelector("h3"); //방에서는 제목이 바뀜
+    h3.innerText = `Room ${roomName}`;
+    //메시지 전송(채팅)
+    const msgform = room.querySelector("#msg");
+    msgform.addEventListener("submit" , handleMessageSubmit);
+}
+
+//방 입장시 일어나는 이벤트들
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    //룸은 보내는 것이 아닌 emit 해주면 된다함
+    socket.emit("enter_room" ,  input.value , showRoom );
+    roomName = input.value;
+    input.value = "";
+}
+
 form.addEventListener("submit", handleRoomSubmit);
+
+//서버에서 실행할 수 있게함 -> 룸안에 있는 사람들에게 emit(방출)하는걸
+socket.on("welcome", (user)=>{
+    addMessage(`${user}입장!!`);
+});
+
+//방에 누가 나갈걸 알려주는 메시지
+socket("bye", (left)=> {
+    addMessage(`${left}나감 ㅜ.ㅜ`);
+} );
+
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -31,12 +31,33 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection" , (socket) => {
-  socket.on("enter_room" , (msg, done) => {
-    console.log(msg);
-    setTimeout(() => {
-      done();
-    } , 1000);
+  socket["nickname"] = "Anon";
+  //onAny -> 언제,어디서나 사용 가능하게 해주는 것
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
   });
+
+  //done이 프론트에서 보낸 벡엔드를 실행시키는 것
+  socket.on("enter_room" , (roomName , done) => {
+   socket.join(roomName);//누군가 참여한 걸 알려줌
+   done();
+   //룸안에 있는 사람들에게 emit(방출)함 + 닉네임과 함께
+   socket.to(roomName).emit("welcome", socket.nickname); 
+});
+//누군가 방을 나간 걸 알려줌
+socket.on("disconnecting", () => {
+  socket.rooms.forEach((room) => 
+  socket.to(room).emit("bye", socket.nickname)
+  );
+});
+
+//메시지(채팅) + room을 넣으면서 어디에 메시지를 보내는 지 알게 됨
+socket.on("new_message" , (msg,room,done) => {
+  socket.to(room).emit("new_message" , `${socket.nickname}:${msg}`);
+  done();
+});
+socket.on("nickname", (nickname) =>
+(socket["nickname"]= nickname));
 });
 
 const handleListen = () =>
@@ -52,7 +73,7 @@ httpServer.listen(3000,handleListen);
 
 
 
-/* 소켓 -> 소켓 io 사용하기 위함 
+/* 소켓 -> 소켓 io 사용하기 위해 주석함
 //페이크 디비 생성 (모든 브라우저가 소켓을 받게 하기 위함)
 const sockets = [];
 
